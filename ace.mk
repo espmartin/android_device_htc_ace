7809afce9979417340b1ee2daecf5c36746f972a
Martin Espericueta <espmartin@users.noreply.github.com>
2014-12-13 12:32:52 -0800

    Update ace.mk

--- ace.mk
+++ ace.mk
@@ -13,3 +13,10 @@ PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ace BUILD_FINGERPRINT="htc_wwe/htc_ace/ace:$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date +%Y%m%d%H%M%S):$(TARGET_BUILD_VARIANT)/test-keys" PRIVATE_BUILD_DESC="htc_ace-$(TARGET_BUILD_VARIANT) $(PLATFORM_VERSION) $(BUILD_ID) $(shell date +%Y%m%d%H%M%S) test-keys"
+
+# Release Name
+PRODUCT_RELEASE_NAME := ace
+
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
